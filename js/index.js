e72e5686d5c66e7cc5b89b40ca8215bd71d22d72
Emmanuel Lagunas <emanuel_le_dcn@hotmail.es>
2020-07-17 11:12:03 -0500

    primer commit para subirlo a git

---
+++ js/index.js
@@ -0,0 +1,70 @@
+/*
+rojo
+background: #C93D31;
+
+
+azul
+background: #598ED6;
+
+
+verde
+background: #18AD0F;
+
+
+negro
+background: #6C6A6A;
+
+*/
+
+function cambia_tema(elemento){
+	console.log("Elemento: ", elemento);
+	let color_tema = elemento.style.background;
+
+	// .cabecera_cv
+	let cabecera_cv = document.querySelectorAll(".cabecera_cv");
+	for (var i = 0; i < cabecera_cv.length; i++) {
+		let cb_cv = cabecera_cv[i];
+		cb_cv.style.background = color_tema;
+	}
+
+	// .redes_sociales ul li
+	let redes_sociales = document.querySelectorAll(".redes_sociales ul li");
+	for (var i = 0; i < redes_sociales.length; i++) {
+		let red_social = redes_sociales[i];
+		red_social.style.background = color_tema;
+	}
+
+	// .titulo_info
+	let titulos_info = document.querySelectorAll(".titulo_info");
+	for (var i = 0; i < titulos_info.length; i++) {
+		let titulo_info = titulos_info[i];
+		titulo_info.style.color = color_tema;
+	}
+
+	// .titulo_experiencia_sup
+	let titulos_experiencia_sup = document.querySelectorAll(".titulo_experiencia_sup");
+	for (var i = 0; i < titulos_experiencia_sup.length; i++) {
+		let titulo_experiencia_sup = titulos_experiencia_sup[i];
+		titulo_experiencia_sup.style.color = color_tema;
+	}
+
+	// .titulo_experiencia
+	// .fecha_experiencia
+	let titulos_experiencia = document.querySelectorAll(".titulo_experiencia");
+	for (var i = 0; i < titulos_experiencia.length; i++) {
+		let titulo_experiencia = titulos_experiencia[i];
+		titulo_experiencia.style.borderBottom = "1px solid " + color_tema;
+	}
+
+	let fechas_experiencia = document.querySelectorAll(".fecha_experiencia");
+	for (var i = 0; i < fechas_experiencia.length; i++) {
+		let fecha_experiencia = fechas_experiencia[i];
+		fecha_experiencia.style.borderBottom = "1px solid " + color_tema;
+	}
+
+	let titulos_area_interes = document.querySelectorAll(".titulo_area_interes");
+	for (var i = 0; i < titulos_area_interes.length; i++) {
+		let titulo_area_interes = titulos_area_interes[i];
+		titulo_area_interes.style.color = color_tema;
+	}
+}
